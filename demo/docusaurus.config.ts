9131e8c24fb3b115aeacb22b81a1a1bf56790322
Manoel Valerio da Silveira Neto <manoel.silveira@ebanx.com>
2024-09-27 10:48:52 -0300

    docs

--- demo/docusaurus.config.ts
+++ demo/docusaurus.config.ts
@@ -100,6 +100,10 @@ const config: Config = {
               to: "/pay/direct/refund-orcancel",
             },
             {
+              label: "Resend Bank Info",
+              to: "/pay/direct/resend-bank-info",
+            },
+            {
               label: "Get FX rate",
               to: "/pay/exchange/currency-exchange-conversion",
             },
